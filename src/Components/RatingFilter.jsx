48a92a32c232c47639754b2316954b8f33b9f270
Mohamed <mr.tabal@yahoo.co.uk>
2020-02-03 17:02:45 +0000

    add form to rating filter comp along with the state containing the current rating

--- src/Components/RatingFilter.jsx
+++ src/Components/RatingFilter.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
 class RatingFilter extends Component {
-    state = {}
+    state = { rating: 0 }
     render() {
-        return (<h3>rating filter</h3>);
+        return (<form><h4>Rating:</h4>
+            <label><input type="radio" name="rating" value={1} />1 Star</label><br />
+            <label><input type="radio" name="rating" value={2} />2 Stars</label><br />
+            <label><input type="radio" name="rating" value={3} />3 Stars</label><br />
+            <label><input type="radio" name="rating" value={4} />4 Stars</label><br />
+            <label><input type="radio" name="rating" value={5} />5 Stars</label>
+        </form>)
     }
 }
 
