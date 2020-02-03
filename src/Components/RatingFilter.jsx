b9a932ce5949fc4be83ae55a3bc9bdb2c4274392
Mohamed <mr.tabal@yahoo.co.uk>
2020-02-03 17:04:37 +0000

    add handleChange method that will soon trigger a filter method passed down through props

--- src/Components/RatingFilter.jsx
+++ src/Components/RatingFilter.jsx
@@ -4,12 +4,18 @@ class RatingFilter extends Component {
     state = { rating: 0 }
     render() {
         return (<form><h4>Rating:</h4>
-            <label><input type="radio" name="rating" value={1} />1 Star</label><br />
-            <label><input type="radio" name="rating" value={2} />2 Stars</label><br />
-            <label><input type="radio" name="rating" value={3} />3 Stars</label><br />
-            <label><input type="radio" name="rating" value={4} />4 Stars</label><br />
-            <label><input type="radio" name="rating" value={5} />5 Stars</label>
+            <label><input type="radio" name="rating" value={1} onChange={this.handleChange} />1 Star</label><br />
+            <label><input type="radio" name="rating" value={2} onChange={this.handleChange} />2 Stars</label><br />
+            <label><input type="radio" name="rating" value={3} onChange={this.handleChange} />3 Stars</label><br />
+            <label><input type="radio" name="rating" value={4} onChange={this.handleChange} />4 Stars</label><br />
+            <label><input type="radio" name="rating" value={5} onChange={this.handleChange} />5 Stars</label>
         </form>)
+    }
+    handleChange = (e) => {
+        const { value } = e.target;
+        this.setState({
+            rating: value
+        })
     }
 }
 
