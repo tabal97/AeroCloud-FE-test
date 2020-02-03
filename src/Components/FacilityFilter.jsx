cf220b17dbacb2eee0e40c42b97d5d201c9dcc74
Mohamed <mr.tabal@yahoo.co.uk>
2020-02-03 17:15:36 +0000

    check if Facility is rendering correctly

--- src/Components/FacilityFilter.jsx
+++ src/Components/FacilityFilter.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class FacilityFilter extends Component {
-    state = {}
+    state = { facility: "" }
     render() {
-        return (<h3>this is the facility filter</h3>);
-    }
+        const { facility } = this.state
+        return (
+            <div>
+                <h4>Facility:</h4>
+                <select value={facility}>
+                    <option selected value="">All</option>
+                    <option value="gym">Gym</option>
+                    <option value="pool">Pool</option>
+                    <option value="car park">Car Park</option>
+                    <option value="free wifi">Free WiFi</option>
+                </select></div>);
+    };
 }
 
 export default FacilityFilter;
