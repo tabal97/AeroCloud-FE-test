3ec011e31962736eefae62c36a0e2a98fffdc5a9
Mohamed <mr.tabal@yahoo.co.uk>
2020-02-03 16:25:22 +0000

    mutation test for when function returns all hotels

--- src/utils/searchByFacility.spec.js
+++ src/utils/searchByFacility.spec.js
@@ -28,4 +28,8 @@ describe('Search By Facility', () => {
         expect(actualValue).toEqual(hotelsArray)
         expect(Array.isArray(actualValue)).toBe(true)
     });
+    it('does not mutate hotelsArray even when no parameters are passed', () => {
+        const actualValue = searchByFacility("", hotelsArray);
+        expect(actualValue).not.toBe(hotelsArray)
+    });
 });
