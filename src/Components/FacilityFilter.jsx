97336616c8b1405a4416d2c482f2c121b1223bcf
Mohamed <mr.tabal@yahoo.co.uk>
2020-02-03 16:48:27 +0000

    add boilerplate code into each component

---
+++ src/Components/FacilityFilter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class FacilityFilter extends Component {
+    state = {}
+    render() {
+        return (  );
+    }
+}
+
+export default FacilityFilter;
---
+++ src/Components/NameFilter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class NameFilter extends Component {
+    state = {}
+    render() {
+        return (  );
+    }
+}
+
+export default NameFilter;
