5b9c09d3b0cadc90203c8f1fae35d824a08dd36b
Mohamed <mr.tabal@yahoo.co.uk>
2020-02-03 16:41:28 +0000

    check is hotel lister is rendering

--- src/Components/HotelLister.jsx
+++ src/Components/HotelLister.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+const hotelsArray = require("../utils/hotels.json");
+// import * as filter from "../utils/utilFuncs"
+
 
 class HotelLister extends Component {
     state = {}
     render() {
-        return (  );
+        return (<h2>hotels will be listed here</h2>);
     }
 }
 
